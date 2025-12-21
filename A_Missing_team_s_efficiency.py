509f90f18d872a63770dc7b9af4a592e11cd0855
Zaf-Mif <zaferanmiftah@gmail.com>
2025-12-21 13:01:26 +0300

    CSEC-Contests round 9

---
+++ A_Missing_team_s_efficiency.py
@@ -0,0 +1,8 @@
+def solve(n,a):
+    # a effieciency of n-1 teams
+    return 
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int,input().split()))
+    print(solve(n,a))
